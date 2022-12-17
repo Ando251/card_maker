f5b073b15e9744851fe6cd86a027e49162967e44
Andrea A Sen <77107925+Ando251@users.noreply.github.com>
2022-12-17 20:55:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,22 +19,28 @@ var fs=require('fs')
 const port=8080
 
 fs.readFile('one.html',function(error,one){
-fs.readFile('signup.html',function(error,signup){
-server=http.createServer(function(req,res){
-    if(req.url=='/'){
-        res.writeHead(200,{"Content-Type":"text/html"});
-        res.write(one);
-        res.end();
-    }
-    else if(req.url=='/signup'){
-        res.writeHead(200,{"Content-Type":"text/html"});
-        res.write(signup);
-        res.end();
-    }
-    else
-        res.end("404 not found");
+    fs.readFile('signup.html',function(error,signup){
+    fs.readFile('log.html',function(error,log){
+    server=http.createServer(function(req,res){
+        if(req.url=='/'){
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.write(one);
+            res.end();
+        }
+        else if(req.url=='/signup'){
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.write(signup);
+            res.end();
+        }
+        else if(req.url=='/login'){
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.write(log);
+            res.end();
+        }
+        else
+            res.end("404 not found");
+    });
+    server.listen(port);
 });
-server.listen(port);
 });
-
 });
